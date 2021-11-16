3adf8ea8f144c6f91c3dec4875c8c33299f9071a
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-11-16 21:58:47 +0000

    Add home layout, upload media

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// import {navToggler, togglerClick} from "./utils.js";
+
+
+function isLargeScreen(){
+    if (window.screen.width <= 768){
+      return `<td class="celltitle">${key}</td>`
+    }
+    else {return ''}
+    
+  }
+ 
+//   navToggler.addEventListener("click", togglerClick);
---
+++ static/js/utils.js
@@ -0,0 +1,7 @@
+export const navToggler = document.querySelector("nav button");
+
+// export function togglerClick() {
+//   const navMenu = document.querySelector("#slideMenu");
+//   navToggler.classList.toggle("toggler-open");
+//   navMenu.classList.toggle("open");
+// }
