29b27d14f6d032d9bc25680487b6a3009339c4fe
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-11-22 11:54:33 +0000

    Update home app to include latest URLs

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('privacy/', views.privacy_view, name="privacy"),
+    path('terms-and-conditions/', views.terms_view, name="terms"),
+    path('contact/', views.contact_view, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "home/index.html")
+
+
+def privacy_view(request):
+    return render(request, "home/privacy.html")
+
+
+def terms_view(request):
+    return render(request, "home/terms.html")
+
+
+def contact_view(request):
+    return render(request, "home/contact.html")
