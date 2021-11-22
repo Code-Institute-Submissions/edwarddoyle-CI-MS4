2a9fe00866e05acdf388e4bfb8b747a3dcbe8834
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-11-22 11:51:30 +0000

    Add contact page html

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// import {navToggler, togglerClick} from "./utils.js";
+import {showLoadingButton, displaySuccess, displayError} from "./utils.js";
 
 
 function isLargeScreen(){
@@ -6,7 +6,62 @@ function isLargeScreen(){
       return `<td class="celltitle">${key}</td>`
     }
     else {return ''}
-    
-  }
+ }
+
+ const copyrightYear = document.querySelector('#copyright');
+ let d = new Date();
+ copyrightYear.innerHTML = `&copy; ${d.getFullYear()} KCC`;
+
+ window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.display = "none";
+    }
+}
+scrollToTop.addEventListener("click", function(){document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;});
  
-//   navToggler.addEventListener("click", togglerClick);
+    const contactForm = document.querySelector('form');
+    const formMessage = contactForm.querySelector('p')
+    
+    contactForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      sendContact(e);
+    });
+  
+    async function sendContact(e) {
+  
+  
+      const classes = ["green", "red", ];
+      classes.forEach(c => {
+        if (formMessage.classList.contains(c)) {
+          formMessage.classList.remove(c);
+        }
+      })
+      formMessage.innerHTML = '';
+      showLoadingButton(e.submitter);
+    
+      let emailBody = [];
+      // Loop over form to get values
+      [...contactForm.elements].forEach((el) => {
+        let name = String(el.name);
+        let value = el.value.trim();
+        emailBody = [...emailBody, {
+          name: name,
+          data: value
+        }];
+      });
+      // Send email 28a03270-231f-4b31-a17f-abab2f78630a"b397417c-4418-46e3-a7e2-70dfe00e541c",
+      await Email.send({
+        SecureToken: "20ab0394-e95f-4d08-b30e-c35d55f7830d",
+        To: "edwardpaul.doyle@gmail.com",
+        From: "edwardpaul.doyle@gmail.com",
+        Subject: "New Website Message",
+        Body: emailBody,
+      }).then(
+        message => message = "OK" ? displaySuccess("Message Recieved, Thank You", formMessage, contactForm, e.submitter, "SUBMIT") : displayError(error, formMessage, e.submitter, "SUBMIT")
+      )
+    };
+    
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,7 +1,39 @@
 export const navToggler = document.querySelector("nav button");
 
-// export function togglerClick() {
-//   const navMenu = document.querySelector("#slideMenu");
-//   navToggler.classList.toggle("toggler-open");
-//   navMenu.classList.toggle("open");
-// }
+export function showLoadingButton(el) {
+    let spinner = `<div class="spinner"></div>`;
+    el.textContent = "";
+    el.insertAdjacentHTML("afterbegin", spinner);
+  }
+// hide loading button
+export function hideLoadingButton(el, elText) {
+    el.textContent = elText;
+    el.querySelectorAll("*").forEach((n) => n.remove());
+  }
+  
+  // display error message
+  export function displayError(message, el, input, submitter, elText) {
+    if (message) {
+      el.classList.add('red');
+      el.innerHTML = `<i class="icon-info"></i> ${message}`;
+      hideLoadingButton(submitter, elText)
+    }
+    if (input) {
+      input.focus();
+    }
+  }
+  
+  // display success message
+  export function displaySuccess(message, el, form, submitter, elText) {
+    setTimeout(function () {
+      el.innerHTML = ""
+      el.classList.remove('green');
+    }, 5000);
+    hideLoadingButton(submitter, elText)
+    if (message) {
+        el.classList.add('green');
+        el.insertAdjacentHTML("beforeend", `<i class="icon-check"></i> ${message}`);
+    }
+    form.reset()
+  }
+  
