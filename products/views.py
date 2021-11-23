e701a1a85372a2515df51d5d2274551ccfea3d32
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-11-23 17:52:59 +0000

    Add search function

--- products/views.py
+++ products/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 def all_products(request):
 
     products = Product.objects.all()
+    categories = Category.objects.distinct()
+    query = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, 'You need to enter a search term!')
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'categories': categories,
+        'search_term': query,
     }
 
     return render(request, "products/products.html", context)
