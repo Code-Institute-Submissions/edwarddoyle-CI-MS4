71be16fd3c43ede80943856e99a0407cb5b155e5
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-11-27 21:51:19 +0000

    Update bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -15,10 +15,13 @@ def bag_contents(request):
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
         product_count += quantity
+        sub_total = quantity * product.price
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
-            'product': product,            
+            'product': product,
+            'sub_total': sub_total
+        
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
