a0a1de0f7cd9a280f5c5700a15d71edc26a91b53
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-12-02 00:46:51 +0000

    Add edit and delete function to products

--- products/views.py
+++ products/views.py
@@ -92,9 +92,8 @@ def add_product(request):
 
 
 def edit_product(request, product_id):
-    """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'You must be a spueruser')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -119,3 +118,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'You must be a spueruser')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
