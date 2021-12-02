2177f18ccc7c6f75bfd8567c252690a1e0e933a5
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-12-02 01:37:22 +0000

    Added to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kcc-app-shop.wsgi:application
--- kcc_store/settings.py
+++ kcc_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^!(zg75)3g#a5b%t^2#hjb^+z-*0h06&sd7^a@cs&6mt=v$ud1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kcc-app-shop', 'localhost']
 
 
 # Application definition
@@ -117,12 +117,17 @@ WSGI_APPLICATION = 'kcc_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
