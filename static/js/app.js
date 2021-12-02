8083db03d2e48c176cad47c522798c6a1090178b
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-12-02 22:33:08 +0000

    Tidy up HTMl & fix validation errors

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,35 @@
-import {showLoadingButton, displaySuccess, displayError} from "./utils.js";
+import {
+  showLoadingButton,
+  displaySuccess,
+  displayError
+} from "./utils.js";
 
 
-function isLargeScreen(){
-    if (window.screen.width <= 768){
-      return `<td class="celltitle">${key}</td>`
-    }
-    else {return ''}
- }
-
- const copyrightYear = document.querySelector('#copyright');
- let d = new Date();
- if(copyrightYear){
- copyrightYear.innerHTML = `&copy; ${d.getFullYear()} KCC`;
- }
- window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      scrollToTop.style.display = "block";
-    } else {
-      scrollToTop.style.display = "none";
-    }
+function isLargeScreen() {
+  if (window.screen.width <= 768) {
+    return `<td class="celltitle">${key}</td>`
+  } else {
+    return ''
+  }
 }
-scrollToTop.addEventListener("click", function(){document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
 
+const copyrightYear = document.querySelector('#copyright');
+let d = new Date();
+if (copyrightYear) {
+  copyrightYear.innerHTML = `&copy; ${d.getFullYear()} KCC`;
+}
+window.onscroll = function () {
+  scrollFunction()
+};
 
- 
-   
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+scrollToTop.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
