e23cd66859d51cfd95f7cfa5af73bcb596e06260
edwarddoyle <edwardpaul.doyle@gmail.com>
2022-01-10 21:26:19 +0000

    Add Blog model & views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def all_posts(request):
+    context = Post.objects.filter(status=1).order_by('-created')
+    return render(request, "blog/blog.html", context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, "blog/blog_detail.html", context)
