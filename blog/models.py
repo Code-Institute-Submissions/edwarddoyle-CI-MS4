b9dffdcc3d7420d0ec567b9f40e1a6353ab6b38f
edwarddoyle <edwardpaul.doyle@gmail.com>
2022-01-13 15:17:59 +0000

    Fix blog posts error

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     blog_id = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
